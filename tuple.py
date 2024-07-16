4343dd27018a5ccf4222fee1b11ceb6ce5b5c107
Gautam <gautamkumar929982@gmail.com>
2024-07-16 21:13:27 +0530

    print tupples in tupple.py

--- tuple.py
+++ tuple.py
@@ -2,6 +2,7 @@ tup = (4,1,2,5)
 tup1 = () #this is empty tuple
 # print(tup1)
 # print(type(tup))
-print(tup.index(2))#return index number of given value in function
+print(tup.index(5))#return index number of given value in function
+print(tup)
 
 
